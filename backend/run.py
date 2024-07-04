06df3fc1c53c31aca6e013d35632951407926642
AnuragDahal <freefirepros2020@gmail.com>
2024-07-04 21:46:37 +0545

    chore: Set Flask environment to production in backend run.py

--- backend/run.py
+++ backend/run.py
@@ -4,11 +4,14 @@ import os
 # from core.libs import loadintodb
 
 with app.app_context():
-    from core.models.reportmodel import EnergyReport  
+    from core.models.reportmodel import EnergyReport
     db.create_all()
-    # loadintodb.load_into_db()  
+    # loadintodb.load_into_db()
 
 if __name__ == "__main__":
-    os.environ["FLASK_ENV"] = "development"  # Set the environment variable
-    app.run(host='127.0.0.1', port=5000
-            ) 
+    # os.environ["FLASK_ENV"] = "development"
+    # app.run(host='127.0.0.1', port=5000
+    #         )
+
+    os.environ["FLASK_ENV"] = "production"
+    app.run(host='0.0.0.0', port=5000)
